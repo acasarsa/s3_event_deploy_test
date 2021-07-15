f28473be0d32b6b7d9bd3ae7daf5822c8d63bdc4
Andrew Casarsa <acasarsa@gmail.com>
2021-07-15 16:56:08 -0400

    add a binding.pry

--- app/jobs/s3_event_test_job.rb
+++ app/jobs/s3_event_test_job.rb
@@ -1,8 +1,15 @@
 class S3EventTestJob < ApplicationJob
   s3_event "test-jets-s3-event-job"
   def process
+    binding.pry
     puts "############\n\n\n\n\n"
     puts "s3_event #{JSON.dump(s3_event)}"
     puts "\n\n\n\n\n#############"
+  end
+  
+  s3_event "test-jets-s3-event-job"
+  def self.pro
+    puts "\n\n\n$$$$"
+    puts "in here"
   end
 end
