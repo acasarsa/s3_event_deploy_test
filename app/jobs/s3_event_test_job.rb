d582d399517a49d0e2264aba802fd6fef6b8f9b2
Andrew Casarsa <acasarsa@gmail.com>
2021-07-15 15:56:05 -0400

    s3 event job created and deploying

---
+++ app/jobs/s3_event_test_job.rb
@@ -0,0 +1,9 @@
+class S3EventTestJob < ApplicationJob
+  s3_event "test-jets-s3-event-job"
+
+  def process
+    puts "############\n\n\n\n\n"
+    puts "s3_event #{JSON.dump(s3_event)}"
+    puts "\n\n\n\n\n#############"
+  end
+end
